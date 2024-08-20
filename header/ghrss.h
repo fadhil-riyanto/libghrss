6c7273e9d490dc5ca0b719fb68ddff7a9f37262a
fadhil riyanto <me@fadev.org>
2024-08-20 22:27:02 +0700

    ghrss.h: add CURL ptr
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- header/ghrss.h
+++ header/ghrss.h
@@ -1,14 +1,17 @@
 #ifndef GHRSS_H
 #define GHRSS_H
 
+#include <curl/curl.h>
 #include <stdio.h>
 
 typedef struct gh_rss_ctx {
         char *mem_ptr;
         size_t memsize;
+
+        CURL* curl_ctx;
 } gh_rss_ctx_t;
 
-extern void gh_rss_init(gh_rss_ctx_t *ctx);
-extern void gh_rss_free(gh_rss_ctx_t *ctx);
+void gh_rss_init(gh_rss_ctx_t *ctx);
+void gh_rss_free(gh_rss_ctx_t *ctx);
 
 #endif
