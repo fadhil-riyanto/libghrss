2089567882a0bc76fdbe52070ed92bb1df696060
fadhil riyanto <me@fadev.org>
2024-08-21 08:32:38 +0700

    add curl callback
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- ghrss.cc
+++ ghrss.cc
@@ -1,6 +1,8 @@
+#include <cerrno>
 #include <cstddef>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <stdlib.h>
 #include "header/ghrss.h"
 #include <curl/curl.h>
@@ -13,17 +15,32 @@ static void _init_curl(gh_rss_ctx_t *ctx)
         ctx->curl_ctx = curl_easy_init();
 }
 
-// static size_t curl_write_mem_cb(void* contents, size_t size, size_t n, void *userp)
-// {
-//         size_t realsize = size * n;
+static size_t _curl_write_mem_cb(void* contents, size_t size, size_t n, void *userp)
+{
+        size_t realsize = size * n;
+        
+        struct memory_struct *memstruct = (struct memory_struct*)userp;
 
-// }
+        char* ptr = (char*)realloc(memstruct->mem_ptr, memstruct->memsize + realsize + 1);
+
+        if (!ptr) {
+                perror("realloc");
+                return ENOMEM;
+        }
+
+        memstruct->mem_ptr = ptr;
+        memcpy(&(memstruct->mem_ptr[memstruct->memsize]), contents, realsize);
+        memstruct->memsize = memstruct->memsize + realsize;
+        memstruct->mem_ptr[memstruct->memsize] = '\0';
+        return realsize;
+}
 
 static inline void _peform(gh_rss_ctx_t *ctx, std::string builded_url)
 {
         CURLcode res;
         curl_easy_setopt(ctx->curl_ctx, CURLOPT_URL, builded_url.c_str());
-        curl_easy_setopt(ctx->curl_ctx, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(ctx->curl_ctx, CURLOPT_WRITEFUNCTION, _curl_write_mem_cb);
+        curl_easy_setopt(ctx->curl_ctx, CURLOPT_WRITEDATA, (void*)&ctx->memory_struct);
 
 
         res = curl_easy_perform(ctx->curl_ctx);
@@ -47,7 +64,8 @@ void gh_rss_init(gh_rss_ctx_t *ctx)
         // ctx->mem_ptr = (char*)malloc(1);
         // ctx->memsize =   0;
         ctx->memory_struct.mem_ptr = (char*)malloc(1);
-        ctx->memory_struct.memsize = 1;
+        ctx->memory_struct.memsize = 0;
+
 
         /* init curl */
         curl_global_init(CURL_GLOBAL_ALL);
@@ -62,6 +80,8 @@ void gh_rss_get_updates(gh_rss_ctx_t *ctx, const char* username, const char* rep
 {
         std::string url = build_github_query(username, repo);
         _peform(ctx, url.c_str());
+
+        printf("%s\n", ctx->memory_struct.mem_ptr);
 }
 
 void gh_rss_free(gh_rss_ctx_t *ctx) 
