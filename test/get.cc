73b5b4fe594f18dad1b93f9b960a895625b651fb
fadhil riyanto <me@fadev.org>
2024-09-13 14:30:50 +0700

    test move out the goto statement, and add signal handler

--- test/get.cc
+++ test/get.cc
@@ -1,29 +1,41 @@
 #include "../header/ghrss.h"
+#include <csignal>
 #include <cstdio>
 #include <sys/socket.h>
 #include<unistd.h>
 
+volatile int state = 0;
+
+void signal_cb(int signum)
+{
+        state = 1;
+}
+
 int main()
 {
+        signal(SIGINT, signal_cb);
+
         char* id;
         gh_rss_ctx_t ctx;
         gh_rss_init(&ctx);
         
-        gh_rss_get_updates(&ctx, "grpc", "grpc");
-hitori:
-        gh_rss_get_updates(&ctx, "grpc", "grpc");
-        while (1) {
+        
+        while (!state) {
+                gh_rss_get_updates(&ctx, "grpc", "grpc");
+
                 id = gh_rss_list(&ctx);
                 if (id != 0) {
                         printf("new update: %s\n", id);
                         gh_rss_seen(&ctx, id);
                         free(id);
+
+                        
+                        
                 } else {
-                        break;
+                        sleep(1);
                 }
         }
-        sleep(1);
-        goto hitori;
+
         printf("separator\n");
         
         
